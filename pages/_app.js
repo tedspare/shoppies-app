0ba080b12cecb60f96e7bf863155434eb2c04b6f
Ted Spare <ted.spare@gmail.com>
2021-01-05 11:36:16 -0500

    Add AppProvider required for Polaris

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { AppProvider } from '@shopify/polaris';
+import enTranslations from '@shopify/polaris/locales/en.json';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <AppProvider i18n={enTranslations}>
+      <Component {...pageProps} />
+    </AppProvider>
+  )
 }
 
-export default MyApp
+export default App
