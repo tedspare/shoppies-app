592ee967e90b332877456561d20b8ad8569a7d0b
Ted Spare <ted.spare@gmail.com>
2021-01-06 17:55:53 -0500

    Add nominate button. Improve layout.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useCallback, useState, useEffect } from "react"
 import styles from '../styles/Home.module.css'
-import { Layout, Card, Icon, Link, FormLayout, TextField, List, Stack, Page } from '@shopify/polaris'
+import { Layout, Card, Icon, Link, FormLayout, TextField, List, Stack, Page, Button } from '@shopify/polaris'
 import { SearchMinor } from '@shopify/polaris-icons'
 
 export default function Home() {
@@ -10,6 +10,8 @@ export default function Home() {
   const [results, setResults] = useState([])
 
   const handleQuery = useCallback((newQuery) => setQuery(newQuery), [])
+
+  const handleNominate = (event) => console.log(event)
 
   useEffect(() => {
     if (query.length <= 2) {
@@ -41,36 +43,43 @@ export default function Home() {
         title="The Shoppies"
         style={{ "maxWidth": "900px" }}
       >
-        <Layout>
-          <Layout.Section>
-            <Card sectioned>
-              <FormLayout>
-                <TextField
-                  label="Movie title"
-                  value={query}
-                  onChange={handleQuery}
-                  prefix={<Icon source={SearchMinor} color="inkLighter" />}
-                  placeholder="Search"
-                />
-              </FormLayout>
-            </Card>
-          </Layout.Section>
-          <Layout.Section>
-            <Stack distribution="fill" wrap={false}>
-              <Card title={resultsTitle} sectioned >
+        <Stack vertical>
+          <Card sectioned>
+            <FormLayout>
+              <TextField
+                label="Movie title"
+                value={query}
+                onChange={handleQuery}
+                prefix={<Icon source={SearchMinor} color="inkLighter" />}
+                placeholder="Search"
+              />
+            </FormLayout>
+          </Card>
+          <Layout>
+            <Layout.Section oneHalf>
+              <Card title={resultsTitle} sectioned>
                 <List type="bullet">
                   {results.map((result) => {
-                    return <List.Item key={result.imdbID}>{result.Title} ({result.Year})</List.Item>
+                    return (
+                      <List.Item key={result.imdbID}>
+                        <Stack distribution="equalSpacing" alignment="center">
+                          <p style={{ "word-wrap": "break-word", width: "250px" }}>{result.Title} ({result.Year})</p>
+                          <Button outline size="slim" onClick={handleNominate}>Nominate</Button>
+                        </Stack>
+                      </List.Item>
+                    )
                   })}
                 </List>
                 {results.length == 0 && <p>Keep searching!</p>}
               </Card>
+            </Layout.Section>
+            <Layout.Section oneHalf>
               <Card title="Nominations" sectioned>
                 Nominations
               </Card>
-            </Stack>
-          </Layout.Section>
-        </Layout>
+            </Layout.Section>
+          </Layout>
+        </Stack>
       </Page>
 
       <footer className={styles.footer}>
